a05a050614c097c38c2deba838dd21337522a8ae
Ben Humphris <bhumphris@comcast.net>
2016-11-15 23:12:10 -0600

    Add files via upload

---
+++ 17.3.py
@@ -0,0 +1,14 @@
+class point:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return '(' + str(self.x) + ', ' + str(self.y) + ')'
+
+point = point(5,6)
+
+print point
+
+
